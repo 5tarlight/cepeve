cdee6872b9db25195dc805162e3631fcf59bbcb8
YEAHx4 <developerstarlight@gmail.com>
2023-01-24 01:09:26 +0900

    colorful welcome message

--- console/console.h
+++ console/console.h
@@ -1,4 +1,34 @@
 #pragma once
-#include <iostream>
+
+// https://wiki.archlinux.org/title/Bash/Prompt_customization#Colors
+#define BOLD "\e[1m"
+#define DIM "\e[2m"
+#define ITALIC "\e[3m"
+#define HIGHLIGHT "\e[7m"
+#define GREY "\e[30m"
+#define GRAY "\e[30m"
+#define BG_GRAY "\e[40m"
+#define BG_GREY "\e[40m"
+#define RED "\e[31m"
+#define BG_RED "\e[41m"
+#define GREEN "\e[32m"
+#define BG_GREEN "\e[42m"
+#define YELLOW "\e[33m"
+#define BG_YELLOW "\e[43m"
+#define BLUE "\e[34m"
+#define BG_BLUE "\e[44m"
+#define PURPLE "\e[35m"
+#define BG_PURPLE "\e[45m"
+#define SBLUE "\e[36m" // Sky Blue
+#define BG_SBLUE "\e[46m"
+#define WHITE "\e[37m"
+#define BG_WHITE "\e[47m"
+#define RESET "\e(B\e[m"
 
 void printhello();
+void print(const char *str);
+void print(const char *str, const char *o);
+void print(const char *str, const char *o1, const char *o2);
+void println(const char *str);
+void println(const char *str, const char *o);
+void println(const char *str, const char *o1, const char *o2);
